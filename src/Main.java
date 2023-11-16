fd13c60487ce1ba39e6ebdaee252d223a5843ce0
J_jmc <87952311+Jjmc1313@users.noreply.github.com>
2023-11-16 11:23:45 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
-import java.util.*;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Main {
-  public static void main(String[] args) {
-  
+  public static void main(String[] args) throws Exception {
+    final String baseURL = "http://robertsspaceindustries.com/pledge/ships/";
+    
+    Scanner words = new Scanner(new File("words_alpha.txt"));
+    Writer output = new BufferedWriter(new FileWriter("output.txt", true));
+
+    while (words.hasNext()) {
+      HttpURLConnection con = (HttpURLConnection)(new URL(baseURL + words.next()).openConnection());
+      con.setInstanceFollowRedirects(false );
+      con.connect();
+      int responseCode = con.getResponseCode();
+      System.out.println(responseCode);
+      if (responseCode == 302) {
+        String location = con.getHeaderField("Location");
+        output.append(location);
+      }
+      con.disconnect();
+    }
+    output.close();
   }
 }
